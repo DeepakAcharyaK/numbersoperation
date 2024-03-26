8f8b4d71e4dfbeb635aeac5571250f7b3f05bec4
Deepak Acharya K <129195970+DeepakAcharyaK@users.noreply.github.com>
2024-03-26 22:40:09 +0530

    Commit

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void) {
+  int num1,num2;
+
+  printf("Enter two numbers:");
+  scanf("%d %d",&num1,&num2);
+
+  printf("Sum of two number is %d \n",num1+num2);
+  printf("Product of two number is %d \n",num1*num2);
+  printf("Difference of two number is %d \n",num1-num2);
+  printf("Modulus of two number is %d \n",num1%num2);
+  printf("divide of two number is %d",num1/num2);
+  
+  return 0;
+}
